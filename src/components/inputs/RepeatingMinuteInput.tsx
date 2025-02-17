9153bfee2042dfec52bfcb0eb182cf643f9fa3b0
hesamzkr <hesamaldinzakeri@gmail.com>
2025-02-17 20:04:54 +0100

    fix repeating input to not allow NaN

--- src/components/inputs/RepeatingMinuteInput.tsx
+++ src/components/inputs/RepeatingMinuteInput.tsx
@@ -3,13 +3,25 @@ import { useCronStore } from "../../stores/CronStore";
 function RepeatingMinuteInput() {
   const { minutes, setMinutes } = useCronStore();
 
+  const handleMinutesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value === "") {
+      setMinutes(undefined);
+    } else {
+      const numberValue = Number(value);
+      if (!isNaN(numberValue)) {
+        setMinutes(numberValue);
+      }
+    }
+  };
+
   return (
     <div className="flex items-center gap-2">
       <span>Every</span>
       <input
         className="form-input border w-20 rounded px-2 py-1"
-        value={minutes}
-        onChange={(e) => setMinutes(Number(e.target.value))}
+        value={minutes === undefined ? "" : minutes}
+        onChange={handleMinutesChange}
       />
       <span>Minutes</span>
     </div>
